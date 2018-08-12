a2eb9ce27c7eb646036d5b1171d236adb20616cd
Bastian Kummer <basti@kummer-post.de>
2018-08-13 00:24:39 +0200

    add all code to game

--- main.lua
+++ main.lua
@@ -1,5 +1,5 @@
 local player_speed = 1 / 16
-local player_map_position = {10,10}
+local player_map_position = {10,5}
 local player_move = 0
 local player_tile = 16
 local player_carries_object = nil
@@ -11,16 +11,23 @@ local last_button = -1
 local shredder_position_x = 0
 local shredder_position_y = 0
 local shredder_is_running = 0
-local document_space_x = 1
-local document_space_y = 15
-local document_space_w = 2
-local game_running = true
+local document_space_x = 0
+local document_space_y = 5
+local document_space_w = 3
+local game_running = false
 local object_map = {}
-local garbage_space_x = 25
+local garbage_space_x = 28
 local garbage_space_y =  5
 local garbage_space_w = 2
 local computer_position_x = 0
 local computer_position_y = 0
+local player_score = 0
+
+local document_delay = 60
+local document_amount = 4
+document_next_tick = 0
+
+local garbage_delay = 120
 
 
 local function get_player_map_index()
@@ -55,7 +62,6 @@ local function draw_player()
       spr(256 + player_tile + 96, x + 3, y -4, 0)
     end
   end
-  print("map_x = " .. map_x .. " map_y = " .. map_y)
 end
 
 local function object_map_get(map_x, map_y)
@@ -200,6 +206,7 @@ local shredder_old_id = 0
 local shredder_new_id = 0
 function update_shredder()
   if shredder_is_running > 0 then
+    sfx(1, 0, -1, 1, 7)
     shredder_is_running = shredder_is_running - 1
     if (shredder_is_running <= 210) then
       shredder_new_id = 32 + (math.floor((210 - shredder_is_running) / 30) % 7) * 2
@@ -226,6 +233,7 @@ function update_shredder()
       end
     end
   else
+    sfx(1, 0, 0, 1, 7)
     object_map_put_shredder(384)
     local object = nil
     if not object then
@@ -264,8 +272,9 @@ function update_shredder()
 end
 
 function update_computer()
-  for y = computer_position_y, computer_position_y + 2 do
-    for x = computer_position_x, computer_position_x + 2 do
+  local ret = false
+  for y = computer_position_y, computer_position_y + 1 do
+    for x = computer_position_x, computer_position_x + 1 do
       local array = object_map_get(x, y)
       if (array) then
         for i = 1, #array do
@@ -275,21 +284,21 @@ function update_computer()
             end
             if array[i][2].scanned ~= true then
               array[i][2].scanned = true
+              player_score = player_score + 5
+              ret = true
             end
           end
         end
       end
     end
   end
+  if ret then
+    sfx(0, 60, 30)
+  end
+  return ret
 end
 
-object_map_put_shredder(384, 10, 5)
-object_map_put_computer(464, 15, 5)
 
-object_map_put(5,9,{256})
-object_map_put(5,8,{257})
-object_map_put(5,7,{258})
-object_map_put(5,7,{259})
 
 local function my_sort(a, b)
   return a[1] < b[1]
@@ -338,6 +347,7 @@ local function player_action_put_object()
   local map_x, map_y = player_looking_at()
   if object_map_put(map_x, map_y, player_carries_object) then
     player_carries_object = nil
+    sfx(0, 10, 30)
   end
   if not player_can_move(table.unpack(player_map_position)) then
     -- player stock in object ... align
@@ -353,20 +363,34 @@ local function player_action_get_object()
 
   if obj then
     player_carries_object = obj
+    sfx(0, 10, 30)
   end
 end
 
 local function free_document_space()
+  local ret = document_space_w * document_space_w * 3
+  for x = document_space_x, document_space_x + document_space_w  - 1 do
+    for y = document_space_y, document_space_y + document_space_w - 1 do
+      local array = object_map_get(x,y)
+      if array then
+        for i = 1, #array do
+          ret = ret - 1
+        end
+      end
+    end
+  end
+  return ret
 end
 
 local function new_documents_arrive(cnt)
   local max_rounds = 5
   while cnt > 0 and max_rounds > 0 do
     max_rounds = max_rounds - 1
-    for x = document_space_x, document_space_x + document_space_w do
-      for y = document_space_y, document_space_y + document_space_w do
+    for x = document_space_x, document_space_x + document_space_w - 1 do
+      for y = document_space_y, document_space_y + document_space_w - 1 do
         if(cnt > 0 and not collide_with_object(player_map_position[1], player_map_position[2], x, y)) then
-          if (object_map_put(x, y, {256})) then
+          if (object_map_put(x, y, {259, {scanned=false, expiration=tick + 60 * 60 + 60 * 60 * 10 * math.random() }})) then
+            player_score = player_score + 10
             cnt = cnt - 1
           end
         end
@@ -377,12 +401,14 @@ local function new_documents_arrive(cnt)
 end
 
 local function run_garbage_collection()
-  for x = garbage_space_x, garbage_space_x + garbage_space_w do
-    for y = garbage_space_y, garbage_space_y + garbage_space_w do
+  for x = garbage_space_x, garbage_space_x + garbage_space_w - 1 do
+    for y = garbage_space_y, garbage_space_y + garbage_space_w -1 do
       while true do
         local obj = object_map_take(x, y)
         if (obj == nil or obj[1] ~= 264) then
           break
+        else
+          player_score = player_score + 20
         end
       end
     end
@@ -395,7 +421,16 @@ local function update_objects()
       for i = 1, #val do
         if val[i][2] then
           if val[i][2].scanned == true then
-            val[i][1] = 256
+            if (val[i][2].expiration) then
+              local time_left = val[i][2].expiration - tick
+              if (time_left > 60 * 60 * 2) then
+                val[i][1] = 258 -- green
+              elseif (time_left > 0) then
+                val[i][1] = 257 -- yellow
+              else
+                val[i][1] = 256 -- red
+              end
+            end
           end
         end
       end
@@ -403,15 +438,48 @@ local function update_objects()
   end
 end
 
+
+local function game_init()
+  game_running = true
+  tick = 0
+  player_score = 0
+  player_map_position = {10, 5}
+  player_tile = 16
+  layer_carries_object = nil
+  shredder_is_running = 0
+  document_delay = 60
+  object_map = {}
+  object_map_put_shredder(384, 21, 2)
+  object_map_put_computer(464, 10, 2)
+
+  for i = 6, 20, 1 do
+    for c = 1, 1 + math.random(2) do
+      object_map_put(i,9,{259, {scanned = math.random(2) == 1, expiration = 60 * 60 * 10 * math.random()}})
+    end
+  end
+  for i = 6, 20, 2 do
+    for c = 1, 1 + math.random(2) do
+      object_map_put(i,8,{259, {scanned = math.random(2) == 1, expiration = 60 * 60 * 10 * math.random()}})
+    end
+  end
+end
+
+
 local function do_game()
   -- get user input
   tick = tick + 1
   player_move = 0
 
-  if tick % 500 == 0 then
-    if not new_documents_arrive(4) then
+  if tick >= document_next_tick then
+    if not new_documents_arrive(document_amount) then
       return "game over"
     end
+    document_amount = 1 + math.floor(math.random() * 15)
+    document_delay = document_delay * 0.99
+    document_next_tick = tick + document_delay * 60
+  end
+
+  if tick & (garbage_delay * 60) == 0 then
     run_garbage_collection()
   end
 
@@ -478,19 +546,69 @@ local function do_game()
   update_shredder()
 
   draw_objects()
+  local free_space = free_document_space()
+  local color = 11
+  if (free_space - document_amount > document_amount) then
+    color = 11
+  elseif (free_space - document_amount > 0 ) then
+    color = 14
+  else
+    color = 6
+  end
+  print ("" .. document_amount .. " Documents in ".. math.floor((document_next_tick - tick) / 60)
+  .. "\nSpace: " .. free_space, 0 , 0, color)
+
+  print ("Score: " .. player_score, 140, 0, 11)
+
+  print ("Clear\nin " .. math.floor((garbage_delay * 60 - tick % (garbage_delay * 60)) / 60),
+  205, 75, 5)
 
   --draw_player(table.unpack(player_map_position))
   --spr(256 + player_tile + player_animation, x, y, 15)
   --spr(256 + player_tile + 16 + player_animation, x, y + 8, 15)
 end
 
-function TIC()
-    if game_running then
-     if do_game() then
-       game_running = false;
-     end
-   else
-    print ("Game over", 50, 50)
+local function put_comp(x, y, scale)
+  spr(464, x, y, 0, scale)
+  spr(465, x + 8 * scale, y, 0, scale)
+  spr(480, x, y + 8 *scale, 0, scale)
+  spr(481, x + 8 * scale, y + 8 * scale, 0, scale)
+  print("SHREDDER", 25, 0, 6, true, 4)
+  print("Boy", 85, 25, 6, true, 4)
+end
 
+local function do_title()
+  cls(2)
+  spr(272, 50, 50, 15, 4)
+  spr(288, 50, 50 + 8 * 4, 15, 4)
+
+  put_comp(100,50, 4)
+
+end
+
+local function do_game_over()
+    print ("Game Over", 50, 0, 2, false, 2)
+    print ("No Space Left For Documents", 20, 12, 2, false, 1)
+    print (string.format("your score was: %d", player_score), 20, 30, 2, false, 1)
+    print (string.format("you survived: %d minutes and %d seconds", math.floor(tick / (60 * 60)), math.floor((tick % (60 * 60)) / 60)), 20, 40, 2, false, 1)
+end
+local first_start = true
+function TIC()
+  if first_start then
+    do_title()
+    if btn(4) or btn(1) or btn(0) or btn(2) or btn(3) then
+      first_start = false
+      game_init()
+    end
+  elseif game_running then
+    if do_game() then
+      game_running = false;
+    end
+  else
+    do_game_over()
+    if btn(4) or btn(1) or btn(0) or btn(2) or btn(3) then
+      game_init()
+
+    end
   end
 end
